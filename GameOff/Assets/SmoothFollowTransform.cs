f481cce6c005971e92a904bca36a89e98d5a2f8a
Unknown <cmolln10@student.aau.dk>
2018-11-03 14:04:27 +0100

    Camera

---
+++ GameOff/Assets/SmoothFollowTransform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollowTransform : MonoBehaviour
+{
+	[SerializeField]
+	Transform followTransform;
+
+	Vector3 originalPosition;
+
+	Vector3 speed = Vector3.zero;
+
+	[SerializeField]
+	[Range(0f, 1f)]
+	float smoothTime = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+		originalPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		transform.position = Vector3.SmoothDamp(transform.position, followTransform.position + originalPosition, ref speed, smoothTime);
+    }
+}
