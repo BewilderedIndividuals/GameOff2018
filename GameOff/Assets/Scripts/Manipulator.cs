68590d2250c9933a53ddc8ac1b14070523cf3467
Mads Bock Christensen <bockchristensen@gmail.com>
2018-11-03 13:08:13 +0100

    Manipulator Script

---
+++ GameOff/Assets/Scripts/Manipulator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manipulator : MonoBehaviour
+{
+    public Item.Type[] inputItems;
+    public Item.Type outputItem; //TODO Maybe more items?
+}
