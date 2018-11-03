39d9dc97e4446bc71604f9ed4e4d6f168d2bf30a
Unknown <cmolln10@student.aau.dk>
2018-11-03 14:53:51 +0100

    ScriptableObjects Done, Items Translator Done

--- GameOff/Assets/Scripts/Item.cs
+++ GameOff/Assets/Scripts/Item.cs
@@ -2,18 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour, IInteractable
+
+[CreateAssetMenu(fileName = "Item", menuName = "Items/Base", order = 1)]
+public class Item : ScriptableObject, IInteractable
 {
+    public Type type = Type.Type1;
+	[SerializeField]
+	Texture tex = null;
+	[SerializeField]
+	Material mat = null;
 
 
-    public Type type;
-    public Item Interact(Item heldItem = null)
+    public Item Interact(Item item = null)
     {
         throw new System.NotImplementedException();
-    }
+	}
+
+	public Item ToItem(Item.Type type)
+	{
+		return ItemExtensions.instance.TypeToItem(type);
+	}
 
     public enum Type
     {
-
+		Type1,
+		Type2
     }
+	
 }
