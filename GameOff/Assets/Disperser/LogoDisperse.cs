86bf05cd24c77b8e394eb2c84b8ca88c5692ec53
Møllnitz <cmolln10@student.aau.dk>
2018-11-03 01:20:09 +0100

    Logo, dispersal, menu

---
+++ GameOff/Assets/Disperser/LogoDisperse.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LogoDisperse : MonoBehaviour
+{
+
+    [SerializeField]
+    private Texture2D logoTex;
+
+    [SerializeField]
+    private Material logoMat;
+
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float AlphaValue = 0.5f;
+
+    [SerializeField]
+    private Gradient Gradient;
+
+    [SerializeField]
+    private Gradient GradientTwo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    public Texture2D GetTex()
+    {
+        return logoTex;
+    }
+
+    public Material GetMat()
+    {
+        return logoMat;
+    }
+
+    public float GetAlpha()
+    {
+        return AlphaValue;
+    }
+
+    public Gradient GetGradient()
+    {
+        return Gradient;
+    }
+
+    public Gradient GetGradientTwo()
+    {
+        return GradientTwo;
+    }
+}
---
+++ GameOff/Assets/Disperser/ParticleReciever.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleReciever : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool OverrideRendererMaterial = false;
+
+    [SerializeField]
+    private bool OverrideShape = false;
+
+    [SerializeField]
+    private bool OverrideAlpha = false;
+
+    [SerializeField]
+    private bool OverrideColorOverLifetime = false;
+
+    [SerializeField]
+    private bool OverrideColorOverLifetimeTwo = false;
+
+
+    ParticleSystem pSystem;
+
+    LogoDisperse top;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        top = GetComponentInParent<LogoDisperse>();
+        pSystem = GetComponent<ParticleSystem>();
+
+
+        if(OverrideRendererMaterial)
+        {
+            pSystem.GetComponent<ParticleSystemRenderer>().material = top.GetMat();
+        }
+
+        if(OverrideShape)
+        {
+            ParticleSystem.ShapeModule shapemod = pSystem.shape;
+            shapemod.texture = top.GetTex();
+        }
+
+        if(OverrideAlpha)
+        {
+            Color gradientColor = pSystem.main.startColor.color;
+            ParticleSystem.MainModule mainmod = pSystem.main;
+            mainmod.startColor = new Color(gradientColor.r, gradientColor.g, gradientColor.g, top.GetAlpha());
+
+        }
+
+        if (OverrideColorOverLifetime)
+        {
+            ParticleSystem.ColorOverLifetimeModule col = pSystem.colorOverLifetime;
+            col.color = top.GetGradient();
+        }
+
+        if (OverrideColorOverLifetimeTwo)
+        {
+            ParticleSystem.ColorOverLifetimeModule col = pSystem.colorOverLifetime;
+            col.color = top.GetGradientTwo();
+        }
+    }
+}
