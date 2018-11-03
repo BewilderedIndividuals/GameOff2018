6434f488bf81974b4f91fe802a9f18c9e39f72eb
Mads Bock Christensen <bockchristensen@gmail.com>
2018-11-03 13:11:46 +0100

    Objective Script

---
+++ GameOff/Assets/Scripts/Objective.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : MonoBehaviour
+{
+    public Item.Type itemRequired;
+    public int count;
+
+}
